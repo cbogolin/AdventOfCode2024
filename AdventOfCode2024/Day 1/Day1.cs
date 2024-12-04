93093c4292f63d475d30d339e6ec15536dcc2f64
cbogolin <chandlerbogolin@gmail.com>
2024-12-04 16:14:03 -0500

    Completed Day 1 part 1

---
+++ AdventOfCode2024/Day 1/Day1.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2024.Day_1;
+
+public static class Day1
+{
+    public static List<int> SortList(List<int> list)
+    {
+        return list.OrderBy(x => x).ToList();
+    }
+    
+    public static int GetTotalDiffs(List<int> list1, List<int> list2)
+    {
+        return list1.Select(
+            (t, i) => Math.Abs(t - list2[i])
+            ).Sum();
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AdventOfCode2024.Day_1;
+
 Console.WriteLine("Hello, World!");
+
+var sortedList1 = Day1.SortList(Inputs.List1);
+var sortedList2 = Day1.SortList(Inputs.List2);
+
+Console.WriteLine($"GetTotalDiffs() = {Day1.GetTotalDiffs(sortedList1, sortedList2)}");
