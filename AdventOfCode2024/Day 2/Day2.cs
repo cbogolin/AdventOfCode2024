2c48a8bec75c979fa0b23f28c7094ac3e98e6195
cbogolin <chandlerbogolin@gmail.com>
2024-12-06 16:27:07 -0500

    completed Day 2 part 2

--- AdventOfCode2024/Day 2/Day2.cs
+++ AdventOfCode2024/Day 2/Day2.cs
@@ -5,21 +5,41 @@ public class Day2 : AdventDayBase
     public override void Run()
     {
         var data = File.ReadLines(@"Day 2\Day2Input.txt");
-        var reportsList = data.Select(line => line.Split(' ').Select(int.Parse).ToArray()).ToList();
+        var reportsList = data.Select(line => line.Split(' ').Select(int.Parse).ToList()).ToList();
 
         var safeReports = GetCountOfSafeReports(reportsList);
         Console.WriteLine($"SafeReports: {safeReports}");
+        
+        safeReports = GetCountOfSafeReportsWithDampener(reportsList);
+        Console.WriteLine($"SafeReportsWithDampener: {safeReports}");
     }
 
-    private static int GetCountOfSafeReports(List<int[]> list)
+    private static int GetCountOfSafeReports(List<List<int>> list)
     {
         return list.Count(IsReportSafe);
     }
+    
+    private static int GetCountOfSafeReportsWithDampener(List<List<int>> list)
+    {
+        return list.Count(IsReportSafeWithDampener);
+    }
 
-    private static bool IsReportSafe(int[] report)
+    private static bool IsReportSafeWithDampener(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            var tempReport = new List<int>(list);
+            tempReport.RemoveAt(i);
+            if (IsReportSafe(tempReport)) return true;
+        }
+
+        return false;
+    }
+    
+    private static bool IsReportSafe(List<int> report)
     {
         bool? valueIncreasing = null;
-        for (var i = 0; i < report.Length - 1; i++)
+        for (var i = 0; i < report.Count - 1; i++)
         {
             //check if numbers change
             if (report[i] == report[i + 1])
