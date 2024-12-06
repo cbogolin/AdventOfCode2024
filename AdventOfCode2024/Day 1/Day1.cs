01f49b2b643895179403770eed3ad80763db8c32
cbogolin <chandlerbogolin@gmail.com>
2024-12-06 14:49:03 -0500

    refactor for better organization

--- AdventOfCode2024/Day 1/Day1.cs
+++ AdventOfCode2024/Day 1/Day1.cs
@@ -2,19 +2,28 @@
 
 public static class Day1
 {
-    public static List<int> SortList(List<int> list)
+    public static void Run()
+    {
+        var sortedList1 = SortList(Inputs.List1);
+        var sortedList2 = SortList(Inputs.List2);
+
+        Console.WriteLine($"GetTotalDiffs() = {GetTotalDiffs(sortedList1, sortedList2)}");
+        Console.WriteLine($"GetSimilarityScore() = {GetSimilarityScore(sortedList2, sortedList1)}");
+    }
+
+    private static List<int> SortList(List<int> list)
     {
         return list.OrderBy(x => x).ToList();
     }
     
-    public static int GetTotalDiffs(List<int> list1, List<int> list2)
+    private static int GetTotalDiffs(List<int> list1, List<int> list2)
     {
         return list1.Select(
             (t, i) => Math.Abs(t - list2[i])
             ).Sum();
     }
 
-    public static int GetSimilarityScore(List<int> list1, List<int> list2)
+    private static int GetSimilarityScore(List<int> list1, List<int> list2)
     {
         var score = 0;
         var groupedList2 = list2.GroupBy(x => x).ToList();
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -2,10 +2,57 @@
 
 using AdventOfCode2024.Day_1;
 
+const AdventDay currentDay = AdventDay.Day1;
+
 Console.WriteLine("Hello, World!");
 
-var sortedList1 = Day1.SortList(Inputs.List1);
-var sortedList2 = Day1.SortList(Inputs.List2);
+switch (currentDay)
+{
+    case AdventDay.Day1:
+    {
+        Day1.Run();
+        break;
+    }
+    case AdventDay.Day2:
+    {
+        //Day2.Run();
+        break;
+    }
+    default:
+        break;
+}
 
-Console.WriteLine($"GetTotalDiffs() = {Day1.GetTotalDiffs(sortedList1, sortedList2)}");
-Console.WriteLine($"GetSimilarityScore() = {Day1.GetSimilarityScore(sortedList2, sortedList1)}");
+internal enum AdventDay
+{
+    Day1,
+    Day2,
+    Day3,
+    Day4,
+    Day5,
+    Day6,
+    Day7,
+    Day8,
+    Day9,
+    Day10,
+    Day11,
+    Day12,
+    Day13,
+    Day14,
+    Day15,
+    Day16,
+    Day17,
+    Day18,
+    Day19,
+    Day20,
+    Day21,
+    Day22,
+    Day23,
+    Day24,
+    Day25,
+    Day26,
+    Day27,
+    Day28,
+    Day29,
+    Day30,
+    Day31
+}
